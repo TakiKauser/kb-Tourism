afa32755b04b7ac9b04362c96bea4c99f82744dc
TakiKauser <takikauserbadhon@gmail.com>
2021-10-31 15:31:23 +0600

    spinner

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Spinner } from 'react-bootstrap';
 import useEvents from '../../hooks/useEvents';
 import Event from '../Event/Event';
 import './Events.css';
@@ -9,14 +9,19 @@ const Events = () => {
     // console.log(events);
     return (
         <div className='container my-4'>
-            <Row xs={1} md={3} className="g-3">
-                {
-                    events?.map(event => <Event
-                        key={event._id}
-                        event={event}
-                    ></Event>)
-                }
-            </Row>
+            {
+                events.length === 0 ?
+                    <Spinner animation="grow" variant="dark" />
+                    :
+                    <Row xs={1} md={3} className="g-3">
+                        {
+                            events?.map(event => <Event
+                                key={event._id}
+                                event={event}
+                            ></Event>)
+                        }
+                    </Row>
+            }
         </div>
     );
 };
